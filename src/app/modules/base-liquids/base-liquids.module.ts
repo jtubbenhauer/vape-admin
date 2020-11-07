ed8184a791f4f0306755ce8196d5d9c8a1e24066
jtubbenhauer <jack@nimbusvapour.com.au>
2020-11-07 22:51:39 +1100

    dialog stuff

--- src/app/modules/base-liquids/base-liquids.module.ts
+++ src/app/modules/base-liquids/base-liquids.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { BaseLiquidsRoutingModule } from './base-liquids-routing.module';
 import { BaseLiquidsComponent } from './page/base-liquids.component';
 import { SharedModule } from 'app/shared/shared.module';
+import { VgStockDialogComponent } from './dialogs/vg-stock-dialog/vg-stock-dialog.component';
 
 
 @NgModule({
-  declarations: [BaseLiquidsComponent],
+  declarations: [BaseLiquidsComponent, VgStockDialogComponent],
   imports: [
     CommonModule,
     BaseLiquidsRoutingModule,
---
+++ src/app/modules/base-liquids/dialogs/vg-stock-dialog/vg-stock-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vg-stock-dialog',
+  templateUrl: './vg-stock-dialog.component.html',
+  styleUrls: ['./vg-stock-dialog.component.css']
+})
+export class VgStockDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
