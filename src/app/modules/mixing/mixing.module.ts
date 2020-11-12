9ae7f01d01ab44001993a85a78bbede555c82213
Jack <jack@nimbusvapour.com.au>
2020-11-13 03:24:44 +1100

    started mixing module

--- src/app/modules/mixing/mixing.module.ts
+++ src/app/modules/mixing/mixing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MixingRoutingModule } from './mixing-routing.module';
-import { PageComponent } from './page.component';
 import { MixingComponent } from './page/mixing.component';
 
 
 @NgModule({
-  declarations: [PageComponent, MixingComponent],
+  declarations: [MixingComponent],
   imports: [
     CommonModule,
     MixingRoutingModule
