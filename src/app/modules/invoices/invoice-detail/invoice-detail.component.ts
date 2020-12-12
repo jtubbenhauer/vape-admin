82b47b2ab9e06798d50ef7170cbda24f13d49cfd
jtubbenhauer <jack@nimbusvapour.com.au>
2020-12-12 18:48:44 +1100

    working on invoices

--- src/app/modules/invoices/invoice-detail/invoice-detail.component.ts
+++ src/app/modules/invoices/invoice-detail/invoice-detail.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from "@angular/router";
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { FlavoursService } from 'app/data/service/flavours.service';
 import { InvoiceService } from 'app/data/service/invoice.service';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface Flavour {
   name: string;
@@ -17,14 +19,22 @@ export interface Flavour {
   templateUrl: './invoice-detail.component.html',
   styleUrls: ['./invoice-detail.component.css']
 })
-export class InvoiceDetailComponent implements OnInit {
+export class InvoiceDetailComponent implements OnInit, AfterViewInit {
 
   id: string;
   invoice: string;
   suppliers: string[] = [];
   flavourList: Flavour[] = [];
   filteredOptions: Observable<Flavour[]>;
+  productList: any[] = [];
 
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatSort) sort: MatSort;
+  displayedColumns: string[] = ['product', 'unit', 'qty', 'cost', 'total']
+  unitOptions: string[] = ['Millilitre', 'Litre', 'Ounce', '16 Ounce', 'Gallon']
+
+
+  productQtys: any = {};
 
   invoicenum = new FormControl('')
   supplier = new FormControl('')
@@ -32,6 +42,11 @@ export class InvoiceDetailComponent implements OnInit {
   addProduct = new FormControl('')
 
   constructor(private route: ActivatedRoute, private service: InvoiceService, private flavourService: FlavoursService) { }
+
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
@@ -73,28 +88,38 @@ export class InvoiceDetailComponent implements OnInit {
   };
 
   supplierChange(value) {
+    let flavours = new Array();
     this.flavourService.getFlavoursFromSupplier(value).snapshotChanges().subscribe(res => {
       res.map(i => {
-        console.log(i.payload.doc.data()['name']);
-        console.log(i.payload.doc.id);
-        
-      })
-      
+        flavours.push({
+          'name': i.payload.doc.data()['name'],
+          'id': i.payload.doc.id
+        })
+      });
+      this.filteredOptions = this.addProduct.valueChanges.pipe(
+            startWith(''),
+            map(value => typeof value === 'string' ? value: value.name),
+            map(name => name ? this._filter(name) : flavours.slice())
+          )
     })
+  };
 
-    // this.flavourService.getFlavours().subscribe(item => {
-    //   item.map(i => {        
-    //     this.flavourList.push({
-    //       'name': i['supplier'] + ' - ' + i['name'],
-    //       'id': i['id']
-    //     });
-    //   });
-    //   this.filteredOptions = this.addProduct.valueChanges.pipe(
-    //     startWith(''),
-    //     map(value => typeof value === 'string' ? value: value.name),
-    //     map(name => name ? this._filter(name) : this.flavourList.slice())
-    //   )
-    // })
+  addProductHandler() {
+    this.flavourService.getFlavourFromID(this.addProduct.value.id).subscribe(res => {
+      this.productList.push({
+        'product': res['name'],
+        'unit': res['unit'],
+        'cost': res['cost'],
+        'qty': 0,
+        'total': '0',
+        'id': this.addProduct.value.id
+      })
+      this.dataSource.data = this.productList;
+    });
   }
 
+  formFieldChange() {
+    console.log(this.dataSource.data);
+    
+  }
 }
