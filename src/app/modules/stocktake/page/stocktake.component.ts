6408214fab21eaa477a57c9817602368b8e82887
jtubbenhauer <jack@nimbusvapour.com.au>
2021-05-29 17:33:59 +1000

    a

--- src/app/modules/stocktake/page/stocktake.component.ts
+++ src/app/modules/stocktake/page/stocktake.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { map, startWith } from 'rxjs/operators';
+import { FlavoursService } from 'app/data/service/flavours.service';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-stocktake',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StocktakeComponent implements OnInit {
 
-  constructor() { }
+  supplierOptions: string[] = [];
+  flavours: any[] = [];
+  filteredOptions: Observable<any[]>;
+
+  supplier = new FormControl('');
+
+  constructor(private flavourService: FlavoursService) { }
 
   ngOnInit(): void {
+    this.getSuppliers();
+  }
+
+  getSuppliers() {
+    this.flavourService.getSuppliers().subscribe(res => {
+      res.map(i => {
+        this.supplierOptions.push(i['name'])
+      })
+    })
+  }
+
+  supplierChange(value) {
+    this.flavours = [];
+    this.flavourService.getFlavoursFromSupplier(value).snapshotChanges().subscribe(res => {
+      res.map(i => {
+        console.log(i.payload.doc.data())
+        this.flavours.push({
+          'name': i.payload.doc.data()['name'],
+          'id': i.payload.doc.id
+        });
+      });
+    })
   }
 
 }
