636d65e1fc720061f97250652b535cfe407fc413
jtubbenhauer <jack@nimbusvapour.com.au>
2021-06-04 17:04:14 +1000

    stocktake

--- src/app/modules/stocktake/page/stocktake.component.ts
+++ src/app/modules/stocktake/page/stocktake.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { map, startWith } from 'rxjs/operators';
 import { FlavoursService } from 'app/data/service/flavours.service';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-stocktake',
   templateUrl: './stocktake.component.html',
   styleUrls: ['./stocktake.component.css']
 })
-export class StocktakeComponent implements OnInit {
+export class StocktakeComponent implements OnInit, AfterViewInit {
 
   supplierOptions: string[] = [];
   flavours: any[] = [];
   filteredOptions: Observable<any[]>;
+
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatSort) sort: MatSort;
+  displayedColumns: string[] = ['name', 'unit', 'qty', 'newUnit', 'newQty', 'save'];
+  unitOptions: string[] = ['Millilitre', 'Litre', 'Ounce', '16 Ounce', 'Gallon']
 
   supplier = new FormControl('');
 
@@ -21,6 +28,12 @@ export class StocktakeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getSuppliers();
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.dataSource.sort = this.sort;
   }
 
   getSuppliers() {
@@ -31,7 +44,24 @@ export class StocktakeComponent implements OnInit {
     })
   }
 
-  supplierChange(value) {
+  newStocktakeButton() {
+    let supplier = this.supplier.value;
+    this.flavours = [];
+    this.flavourService.getFlavoursFromSupplier(supplier).snapshotChanges().subscribe(res => {
+      res.map(i => {
+        console.log(i.payload.doc.data())
+        this.flavours.push({
+          'name': i.payload.doc.data()['name'],
+          'id': i.payload.doc.id,
+          'stock': i.payload.doc.data()['stock'],
+          'unit':i.payload.doc.data()['unit']
+        });
+      });
+      this.dataSource.data = this.flavours;
+    })
+  }
+
+  /*supplierChange(value) {
     this.flavours = [];
     this.flavourService.getFlavoursFromSupplier(value).snapshotChanges().subscribe(res => {
       res.map(i => {
@@ -41,7 +71,8 @@ export class StocktakeComponent implements OnInit {
           'id': i.payload.doc.id
         });
       });
+      this.dataSource.data = this.flavours;
     })
-  }
+  } */
 
 }
